c7421cff3d5183d16bdb9c26f8e036a1bae32d3b
Muzantip <mangeti@mail.ru>
2018-05-08 12:38:18 +0400

    11

--- assets/js/GameScene/ScrollScript.js
+++ assets/js/GameScene/ScrollScript.js
@@ -15,28 +15,18 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        //массив всех команд, для распределение по скроллам 
+        commands: {
+            default: [],
+            type: cc.Prefab
+        },
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
 
-    start () {
+    start() {
 
     },
 
