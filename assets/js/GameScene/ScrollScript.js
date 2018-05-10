1c7c25a9a94c0ddaf6ebc7a97918cea0ac6c2dcd
Muzantip <mangeti@mail.ru>
2018-05-10 11:45:04 +0400

    11

--- assets/js/GameScene/ScrollScript.js
+++ assets/js/GameScene/ScrollScript.js
@@ -15,8 +15,33 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        //массив всех команд, для распределение по скроллам 
-        commands: {
+        //массив самый легких копанд, первые 4 команды
+        veryLightCommands: {
+            default: [],
+            type: cc.Prefab
+        },
+        //массив из 5 команд
+        LightCommands: {
+            default: [],
+            type: cc.Prefab
+        },
+        // массив из 6 команд
+        MediumCommands: {
+            default: [],
+            type: cc.Prefab
+        },
+        //массив из 10 команд
+        aboveAverageCommands: {
+            default: [],
+            type: cc.Prefab
+        },
+        // массив из 8 команд, циклы условия
+        ComplexCommands: {
+            default: [],
+            type: cc.Prefab
+        }, 
+        // массив из 12 команд, циклы, условия
+        LegendCommands: {
             default: [],
             type: cc.Prefab
         },
@@ -27,7 +52,26 @@ cc.Class({
     // onLoad () {},
 
     start() {
+//        this.test();
+//        var cont = this.node.getChildByName("rightScroll").getChildByName("view").getChildByName("content")
+//        cont.addChild(this.commands[0].data)
+//        cont.addChild(this.commands[1].data)
+//        cont.addChild(this.commands[2].data)
+//        cont.anchorX = 1;
+//        cont.anchorY = 0;
+//        var itemWH = cont.getContentSize().width / 2;
+//        for (var i = 0; i < cont.children.length; i++) {
+//            var child = cont.children[i];
+//            child.anchorX = 1;
+//            child.anchorY = 0;
+//            //  child.setContentSize(itemWH,itemWH);
+//            child.setPosition(cont.getPosition().x, 0)
+//        }
+//        console.log(cont.getContentSize() + " " + this.commands[0].data.getPosition() + " ")
+    },
 
+    test() {
+        //console.log(this.commands)
     },
 
     // update (dt) {},
