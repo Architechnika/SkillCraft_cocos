232a48aaa4032e7712b21307d03b1e63945b86b3
Muzantip <mangeti@mail.ru>
2018-05-10 13:22:40 +0400

    jjj

--- assets/js/GameScene/ScrollScript.js
+++ assets/js/GameScene/ScrollScript.js
@@ -46,28 +46,13 @@ cc.Class({
             type: cc.Prefab
         },
     },
-//
+    //
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
 
     start() {
         this.itemsSort(this.LegendCommands);
-        //        var cont = this.node.getChildByName("rightScroll").getChildByName("view").getChildByName("content")
-        //        cont.addChild(this.commands[0].data)
-        //        cont.addChild(this.commands[1].data)
-        //        cont.addChild(this.commands[2].data)
-        //        cont.anchorX = 1;
-        //        cont.anchorY = 0;
-        //        var itemWH = cont.getContentSize().width / 2;
-        //        for (var i = 0; i < cont.children.length; i++) {
-        //            var child = cont.children[i];
-        //            child.anchorX = 1;
-        //            child.anchorY = 0;
-        //            //  child.setContentSize(itemWH,itemWH);
-        //            child.setPosition(cont.getPosition().x, 0)
-        //        }
-        //        console.log(cont.getContentSize() + " " + this.commands[0].data.getPosition() + " ")
     },
 
     itemsSort(arr) {
@@ -79,7 +64,7 @@ cc.Class({
         cont.anchorX = 1;
         cont.anchorY = 1;
         var itemWH = cont.getContentSize().width / 2;
-        cont.setContentSize(cont.getContentSize().width, itemWH * arr.length/2)
+        cont.setContentSize(cont.getContentSize().width, itemWH * arr.length / 2)
         var x = cont.getPosition().x;
         var y = 0;
         for (var i = 0; i < cont.children.length; i++) {
