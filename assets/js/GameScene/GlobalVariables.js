634643565a68a5369feb814a118e8944aded5e81
Ilya <ilya1591@mail.ru>
2018-05-15 17:21:19 +0300

    bbb

--- assets/js/GameScene/GlobalVariables.js
+++ assets/js/GameScene/GlobalVariables.js
@@ -4,17 +4,14 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-<<<<<<< HEAD
         currentLabSize: 3, //Текущий размер лабиринта
         isDebug: false, //Дебаг режим для всей игры
         _oldSelectRoad: undefined, //Переменная для хранения ссылки на последнюю кликнутую дорогу на поле
         _collisionManager: null,
-=======
         oldSelectRoad: undefined,//Переменная для хранения ссылки на последнюю кликнутую дорогу на поле
         selectedRoad: undefined,
         currentLabSize: 51,//Текущий размер лабиринта
         collisionManager:null,
->>>>>>> bf72e2b8b0f7f1e20f9f0b11e9fef0aa5a608f8f
     },
 
     start() {
