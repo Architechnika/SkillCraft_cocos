0bd85be8c84b5b25108ab8912575538293305876
Muzantip <mangeti@mail.ru>
2018-05-16 16:06:39 +0400

    ррр

--- assets/js/GameScene/GenCodeMap.js
+++ assets/js/GameScene/GenCodeMap.js
@@ -42,12 +42,11 @@ cc.Class({
 
     // onLoad () {},
     onLoad() {
-       // this.declaration();
-       // this.generation();
+        // this.declaration();
+        // this.generation();
     },
 
     start() {
-
     },
     clear() {
         //чистим весь кодмап
@@ -61,41 +60,66 @@ cc.Class({
         this.commands = arr;
     },
     generation() {
-        this.node.anchorX = 0;
-        this.node.anchorY = 1;
-        //  var x = this.node.FBP.dr.x;
-        // var y = this.node.FBP.dr.y;
-        //        var x = this.node.x;
-        //        var y = this.node.y;
-        var itemWH = 100;
-        var iter = 0;
-        this.node.addChild(this.selectParentNode)
-        this.selectParentNode.resetTransform;
-        this.selectParentNode.anchorX = 0;
-        this.selectParentNode.anchorY = 1;
-        var x = this.selectParentNode.x;
-        var y = this.selectParentNode.y;
-        for (var i = 0; i < this.selectParentNode.children.length; i++) {
-            var el = this.selectParentNode.children[i];
-            el.anchorX = 0;
-            el.anchorY = 1;
-            el.x = x;
-            el.y = y;
-            if (el.name == "command_if") {
-                y -= (itemWH * 4)
-                iter += 4;
-            } else {
-                y -= itemWH
-                iter++;
+        var road = this.node.parent.getChildByName("GameNode").getComponent("GlobalVariables").selectedRoad;
+        if (road) {
+            this.clear();
+            var roadCommands = road.getComponent("RoadScript").roadCommands;
+            if (roadCommands.length > 0) {
+                var x = this.node.x;
+                var y = this.node.y;
+                for(var i=0;i<roadCommands.length;i++)
+                    {
+                        var el = roadCommands[i];
+//                        el.anchorX = 0;
+//                        el.anchorY = 2;
+
+                        var itemWH = el.width;
+                        if(el.name == "command_if")
+                            {
+                                itemWH = el.height
+                            }
+                        this.node.addChild(el)
+                        el.resetTransform
+                        el.setPosition(x,y)
+                        y+=itemWH;
+                    }
             }
-            //  this.node.addChild(el)
         }
-        this.node.setContentSize(itemWH * iter, itemWH * iter)
-        var plusCHild = this.node.children[0];
-        plusCHild.x = x;
-        plusCHild.y = this.selectParentNode.y - this.node.height;
-        this.node.scaleX = 0.3
-        this.node.scaleY = 0.3
+        //        this.node.anchorX = 0;
+        //        this.node.anchorY = 1;
+        //        //  var x = this.node.FBP.dr.x;
+        //        // var y = this.node.FBP.dr.y;
+        //        //        var x = this.node.x;
+        //        //        var y = this.node.y;
+        //        var itemWH = 100;
+        //        var iter = 0;
+        //        this.node.addChild(this.selectParentNode)
+        //        this.selectParentNode.resetTransform;
+        //        this.selectParentNode.anchorX = 0;
+        //        this.selectParentNode.anchorY = 1;
+        //        var x = this.selectParentNode.x;
+        //        var y = this.selectParentNode.y;
+        //        for (var i = 0; i < this.selectParentNode.children.length; i++) {
+        //            var el = this.selectParentNode.children[i];
+        //            el.anchorX = 0;
+        //            el.anchorY = 1;
+        //            el.x = x;
+        //            el.y = y;
+        //            if (el.name == "command_if") {
+        //                y -= (itemWH * 4)
+        //                iter += 4;
+        //            } else {
+        //                y -= itemWH
+        //                iter++;
+        //            }
+        //            //  this.node.addChild(el)
+        //        }
+        //        this.node.setContentSize(itemWH * iter, itemWH * iter)
+        //        var plusCHild = this.node.children[0];
+        //        plusCHild.x = x;
+        //        plusCHild.y = this.selectParentNode.y - this.node.height;
+        //        this.node.scaleX = 0.3
+        //        this.node.scaleY = 0.3
     },
     // update (dt) {},
 });
