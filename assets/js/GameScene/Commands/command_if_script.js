fe6a87c41d654f84aa760eb2495dfeb51ac57736
Muzantip <mangeti@mail.ru>
2018-05-17 20:20:41 +0400

    лл

--- assets/js/GameScene/Commands/command_if_script.js
+++ assets/js/GameScene/Commands/command_if_script.js
@@ -11,7 +11,9 @@ cc.Class({
     _H: 400,
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad() {
+        this._H = 400;
+    },
 
     start() {
 
@@ -43,7 +45,7 @@ cc.Class({
             element.y = bott.y - itemWH;
             bott.y -= itemWH;
             this.node.addChild(element);
-            this.node.parent.height += itemWH;
+          //  this.node.parent.height += itemWH;
         }
 
     },
@@ -54,42 +56,59 @@ cc.Class({
                 comm.anchorX = 0;
                 comm.anchorY = 1;
                 var itemWH = comm.height;
+                var h = itemWH;
                 if (comm.name == "command_if") {
                     itemWH = comm.height;
+                    h = comm.getChildByName("command_block_if").height;
                 }
                 commands.height += itemWH;
                 this.node.parent.height += itemWH;
                 var x = 0;
                 var y = 0;
-                for (var i = 0; i < commands.children.length; i++) {
-                    var el = commands.children[i];
-                    el.y -= itemWH;
-                    x = commands.children[commands.children.length - 1].x
-                    y = commands.children[commands.children.length - 1].y
-                }
+                var plus = commands.children[0];
+                x = plus.x
+                y = plus.y
+                plus.y -= itemWH;
+//                var lineCount = itemWH / h;
+//                for (var i = 0; i < lineCount; i++) 
+//                {
+//                    this.addLine();
+//                }
                 commands.anchorX = 0;
                 commands.anchorY = 1;
                 comm.x = x;
-                comm.y = y + itemWH;
+                comm.y = y;
                 commands.addChild(comm);
             }
         }
     },
     update(dt) {
-       // console.log(this.node.parent.parent.name)
-        if (this.node.parent.name != "content" && this.node.parent.parent.name =="commands" && this._H != this.node.parent.height) {
-            
-            //if(this.node.parent.parent && this.node.parent.parent.name == "commands" || this.node.parent.parent.name == "elseCommands")
-            //   {
-            console.log("Dd")
-            var itemWH = this.node.width;
-            var lineCount = this._H / itemWH;
-            this.node.parent.parent.height += this.H
-            for (var i = 0; i < lineCount; i++)
-                this.addLine();
-            this._H = this.node.parent.height;
-            //  }
-        }
+        // console.log(this.node.parent.parent.name)
+                if (this.node.parent.name != "content" && this.node.parent.parent.name == "commands" && this._H != this.node.parent.height) {
+        
+                    //if(this.node.parent.parent && this.node.parent.parent.name == "commands" || this.node.parent.parent.name == "elseCommands")
+                    //   {
+                    console.log("Dd")
+                    var itemWH = this.node.width;
+                    if (this._H)
+                        var d = this.node.parent.height - this._H
+        
+                    var lineCount = 100 / itemWH;
+                    this.node.parent.parent.height += this.H
+                    for (var i = 0; i < lineCount; i++) {
+                        this.node.parent.parent.parent.getComponent("command_if_script").addLine();
+                        for(var j =1;j<this.node.parent.parent.children.length;j++)
+                            {
+                                var el = this.node.parent.parent.children[i]
+                                el.y-=el.height;
+                            }
+                    }
+                    //this.addLine();
+                    this._H = this.node.parent.height;
+                    //  }
+                }
+
+
         //        if (this._H != this.node.parent.height) {
         //            this._H = this.node.parent.height;
         //            var parent = this.node.parent.parent;
