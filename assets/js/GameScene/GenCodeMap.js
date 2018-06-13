2adbbcbba168eb95a248e76df1b95342e6e8538c
Ilya <ilya1591@mail.ru>
2018-06-13 13:13:32 +0300

    mmnm

--- assets/js/GameScene/GenCodeMap.js
+++ assets/js/GameScene/GenCodeMap.js
@@ -82,7 +82,7 @@ cc.Class({
                     el.anchorY = 1;
                     el.scaleX = el.scaleY = this.defaultElementScale;
                     itemWH = (el.height * el.scaleY);
-                    el._parent = null;
+                    //el._parent = null;
                     if (el.width > maxW)
                         maxW = el.width;
                     el.resetTransform;
