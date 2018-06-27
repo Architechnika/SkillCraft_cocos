73cbc5f0c8883368fe8189a46b461f1ff036c3f8
Muzantip <mangeti@mail.ru>
2018-06-27 10:11:55 +0400

    оо

--- assets/js/ScreenController.js
+++ assets/js/ScreenController.js
@@ -15,17 +15,23 @@ cc.Class({
 
     // LIFE-CYCLE CALLBACKS:
     onLoad() {
-        this._setWH(cc.winSize);
-        this._calcScreen(this._wSbuff);
-        this.node.on('mouseup', function (event) {
-            //Отключает отбражение меню на кодмапе если оно активно
-            if (cc.director._globalVariables && cc.director._globalVariables.codeMapMenu && cc.director._globalVariables.codeMapMenu.active){
-                //Если это блок команд то на него это правило не распространяется
-                var spl = event.target.name.split("_")[1];
-                if(spl && spl == "block");
-                else cc.director._globalVariables.codeMapMenu.active = false;
-            }
-        });
+       // this._setWH(cc.winSize);
+        //this._calcScreen(this._wSbuff);
+
+        var size = cc.view.getFrameSize();
+       // cc.view.(size);
+        console.log(size)
+        console.log(cc.view.getVisibleSize())
+        cc.view.setDesignResolutionSize(size)
+;//        this.node.on('mouseup', function (event) {
+//            //Отключает отбражение меню на кодмапе если оно активно
+//            if (cc.director._globalVariables && cc.director._globalVariables.codeMapMenu && cc.director._globalVariables.codeMapMenu.active){
+//                //Если это блок команд то на него это правило не распространяется
+//                var spl = event.target.name.split("_")[1];
+//                if(spl && spl == "block");
+//                else cc.director._globalVariables.codeMapMenu.active = false;
+//            }
+//        });
     },
 
     update(dt) {
@@ -38,21 +44,21 @@ cc.Class({
 
     //Производит перерасчет якорей всех нодов на поле под заданное разрешение
     _calcScreen(wh) {
-        if (wh.width > wh.height) { //Если ширина больше высоты
+        /*if (wh.width > wh.height) { //Если ширина больше высоты
 
         } else { //Если высота больше ширины
             //Определяем сколько процентов - ширина лабиринта от ширины экрана
             cc.director._setScrollVisible(false);
             //this.node.rotation = 90;
         }
-        this._setWH(wh);
+        this._setWH(wh);*/
     },
 
     //Запоминает текущий размер окна
     _setWH(WH) {
-        this._wSbuff = {
+        /*this._wSbuff = {
             width: WH.width,
             height: WH.height,
-        };
+        };*/
     }
 });
